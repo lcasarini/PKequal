1046472a6760cbad6a427821ebb256c56c212c3b
Luciano Casarini <casarini.astro@gmail.com>
2016-05-01 17:47:56 -0300

    Added files via upload

--- FrankenEmu/emu_w0_wa.sh
+++ FrankenEmu/emu_w0_wa.sh
@@ -2,40 +2,43 @@
 
 ####################################################
 #files exe
-pkequal=/home/user/pkequal/pkequal
-emu=/home/user/FrankenEmu/emu.exe
+pkequal=/home/luciano/astro/pkeq-sims/lib/PKequal-master/PKequal/pkequal
+emu=/home/luciano/astro/pkeq-sims/lib/PKequal-master/FrankenEmu/emu.exe
 ####################################################
 
 #input
-echo "enter H0, Tcmb, N_eff (e.g. 67.31 2.7255 3.13)"
-read H0 Tcmb N_eff
-echo "enter Omega_b*h^2, Omega_c*h^2 (e.g. 0.02222 0.1197)"
-read ombh2 omch2
+echo "enter Omega_b*h^2 (e.g. 0.02222)"
+read ombh2
+echo "enter Omega_m*h^2 (e.g. 0.14192)"
+read ommh2
+echo "enter n_s (e.g. 0.9655)"
+read n_s 
+echo "enter H0 (e.g. 67.31)"
+read H0
 echo "enter w0, wa (e.g. -1.0 -0.8)"
 read w0 wa
-echo "enter sigma8, n_s (e.g. 0.829 0.9655)"
-read sigma8 n_s
+echo "enter sigma8 (e.g. 0.829)"
+read sigma8
 echo "enter redshift (e.g. 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.0)"
 read -a zarray
 echo "enter output type (0: Delta^2/k^1.5; 1: Delta^2; 2: P(k))"
 read outype
 
 #pkequal
+omch2=`echo $ommh2 - $ombh2 | bc`
+echo
+echo 'computing equivalent w and sigma_8 ...'
 cat>pkequal.ini<<EOF
-$H0 $Tcmb $N_eff
+$H0 2.7255 3.04
 $ombh2 $omch2
 $w0 $wa
 $sigma8
 ${#zarray[@]} 
 ${zarray[@]} 
 EOF
-echo
-echo 'computing equivalent w and sigma_8 ...'
 $pkequal< pkequal.ini> pkequal.out
 
 #emu
-ommh2=`echo $ombh2 + $omch2 | bc`
-echo
 echo 'computing spectra:'
 while read z w_eq sigma8_eq
 do
@@ -59,4 +62,4 @@ done < "z_w_s8.txt"
 
 echo 'spectra written in emu_z$z.dat files'
 
-rm pkequal.ini emu.ini emu.out 
+#rm pkequal.ini emu.ini emu.out 
